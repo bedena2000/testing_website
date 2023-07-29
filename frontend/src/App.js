fbc6e4be3043a53eb7fdde16369ba6cbc46b1b7e
bedena2000 <bedena2000@gmail.com>
2023-07-29 22:19:39 +0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const App = () => {
+
+  const [inputVal, setInputVal] = useState('');
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3005/api/notes')
+      .then((result) => {
+        setNotes(result.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  const addNote = () => {
+   
+
+    if(inputVal.trim()) {
+      const newObj = {
+        content: inputVal.trim(),
+        important: Math.random() < 0.5
+      };
+
+      axios
+        .post('http://localhost:3005/api/notes', newObj)
+        .then(response => {
+          setNotes(notes.concat(response.data));
+        })
+        .catch(err => console.log(err));
+
+    };
+
+  };  
+
+  return (
+    <div>
+        <input type="text" 
+          onChange={(e) => setInputVal(e.target.value)} 
+          value={inputVal}
+        />
+        <button onClick={addNote}>
+          create
+        </button>
+        <div>
+          {
+            notes && notes.map(item => (
+              <div key={item.id}>
+                <p>content:  {item.content}</p>
+                <p>is it important?: {
+                  item.important ? 'true' : 'false'
+                }</p>
+              </div>
+            ))
+          }
+        </div>
+    </div>
+  )
+  
+};  
+
+export default App;
